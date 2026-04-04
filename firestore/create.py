ea0918f16572ed2346dfe3f638187ccc31bad1e4
shhaoooo <asd0965104898@gmail.com>
2026-04-04 21:22:18 +0800

    firestore

---
+++ firestore/create.py
@@ -0,0 +1,16 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("firestore/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+doc = {
+  "name": "洪唯皓",
+  "mail": "asd0965104898@gmail.com",
+  "lab": 579
+}
+
+doc_ref = db.collection("靜宜資管").document("Wei-Hao Hong")
+doc_ref.set(doc)
