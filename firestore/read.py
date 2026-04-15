4ab33045862e7ea4b97adccac20db2f98d6febd3
shhaoooo <asd0965104898@gmail.com>
2026-04-15 16:39:11 +0800

    修改位置 移除json檔案

--- firestore/read.py
+++ firestore/read.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firestore/serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
--- firestore/read3.py
+++ firestore/read3.py
@@ -5,7 +5,7 @@ import os
 # ===== Firebase 初始化（安全版：避免重複）=====
 if not firebase_admin._apps:
     cred = credentials.Certificate(
-        os.path.join(os.path.dirname(__file__), "../serviceAccountKey.json")
+        os.path.join(os.path.dirname(__file__), "serviceAccountKey.json")
     )
     firebase_admin.initialize_app(cred)
 
