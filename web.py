9c9795d627cf55931463ee0fb13e9a141c7cf32f
shhaoooo <asd0965104898@gmail.com>
2026-05-19 14:21:41 +0800

    聊天機器人

--- web.py
+++ web.py
@@ -41,7 +41,12 @@ def index():
     link += "<a href='/road'>查詢易肇事路口</a><hr>"
     link += "<a href='/weather'>天氣查詢</a><hr>"
     link += "<a href='/rate'>電影分級資料庫更新</a><hr>"
+    link += "<a href='/demo'>聊天機器人</a><hr>"
     return link
+
+@app.route("/demo")
+def demo():
+    return render_template("demo.html")
 
 @app.route("/webhook3", methods=["POST"])
 def webhook3():
